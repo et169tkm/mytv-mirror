76a2bece2f8cc1b8c19e83d893d357b8034400b4
Eric Tang <eric@eric-tang.com>
2015-12-04 10:28:18 +0800

    download loop finished

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+define("URL_TOKEN_JADE", "http://token.tvb.com/stream/live/hls/mobilehd_jade.smil", true/* case-sensitive*/);
+define("URL_TOKEN_JADE_HD", "http://token.tvb.com/stream/live/hls/mobilehd_hdj.smil", true/* case-sensitive*/);
+define("URL_TOKEN_INEWS", "http://token.tvb.com/stream/live/hls/mobilehd_inews.smil", true/* case-sensitive*/);
+define("URL_TOKEN_J2", "http://token.tvb.com/stream/live/hls/mobilehd_j2.smil", true/* case-sensitive*/);
+define("DATA_DIR", "data/", true);
+define("DB_FILENAME", "db.json", true);
+
+define("ENABLE_HTTP_PIPELINING", false, true);
+
--- fetcher.php
+++ fetcher.php
@@ -1,47 +1,115 @@
 <?php
 
-define("URL_TOKEN_JADE", "http://token.tvb.com/stream/live/hls/mobilehd_jade.smil", true/* case-sensitive*/);
-define("URL_TOKEN_JADE_HD", "http://token.tvb.com/stream/live/hls/mobilehd_hdj.smil", true/* case-sensitive*/);
-define("URL_TOKEN_INEWS", "http://token.tvb.com/stream/live/hls/mobilehd_inews.smil", true/* case-sensitive*/);
-define("URL_TOKEN_J2", "http://token.tvb.com/stream/live/hls/mobilehd_j2.smil", true/* case-sensitive*/);
-define("DATA_DIR", "data/", true);
+require "config.php";
 
 $prefix = null;
 $url = null;
 $cookie_jar_path = null;
 $db = null;
 $filetime = 0;
+$effective_url = null;
+$chunklist_url = null;
 
-function fetch_loop() {
-    global $cookie_jar_path, $prefix, $url, $filetime;
-
-    l(__FUNCTION__."()");
-    $filetime = time();
-
+function init() {
+    global $db, $prefix, $url, $cookie_jar_path, $filetime;
     $db = array();
     $prefix = "jade";
     $url = URL_TOKEN_JADE;
     $cookie_jar_path = DATA_DIR.$prefix."-".$filetime.".cookiejar";
-    
-    
-    $cdn_url = get_cdn_url();
-    l($cdn_url);
-    $chunklist_list_and_effective_url = get_chunklist_list_and_effective_url($cdn_url);
+    $filetime = time();
+}
 
+function fetch_loop() {
+    global $cookie_jar_path, $prefix, $url, $filetime, $effective_url, $chunklist_url;
+    l(__FUNCTION__."()");
 
-    $chunklist_list = $chunklist_list_and_effective_url[0];
-    $effective_url = $chunklist_list_and_effective_url[1];
-    $parsed_chunklist_list = parse_chunklist_list($chunklist_list);
-    $chunklist_filename = $parsed_chunklist_list[count($parsed_chunklist_list)-1]["filename"];
-    $chunklist_url = str_replace("playlist.m3u8", $chunklist_filename, $effective_url);
-    $chunklist = download_chunklist($chunklist_url);
-    if ($chunklist) {
-        $chunklist = parse_chunklist($chunklist);
-        download_chunks($chunklist, $effective_url);
+    while (true) {
+        $chunklist = download_chunklist();
+print_r($chunklist);
+        if ($chunklist) {
+            $chunklist = parse_chunklist($chunklist);
+            download_chunks($chunklist, $effective_url);
+        }
+
+        // increment filetime
+        $chunklist_duration = 0;
+        foreach ($chunklist as $chunk) {
+            $chunklist_duration += $chunk["duration"];
+        }
+        
+        $wait_time = ($filetime - 20) - time();
+        if ($wait_time > 0) { // start early download 10 second before this chunk finish
+            l("going to sleep ".$wait_time." seconds");
+            sleep($wait_time);
+        }
     }
 }
 
-function download_chunklist($chunklist_url) {
+function get_cdn_url() {
+    global $url;
+    l(__FUNCTION__."()");
+
+    $c = new_curl($url."?feed");
+    curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($c, CURLOPT_POST, true);
+    curl_setopt($c, CURLOPT_POSTFIELDS, "feed");
+    $json = curl_exec($c);
+    $status = curl_getinfo($c, CURLINFO_HTTP_CODE);
+    curl_close($c);
+
+    if ($status == 200) {
+        $feedinfo = json_decode($json, true);
+        if (isset($feedinfo["url"])) {
+            return $feedinfo["url"];
+        } else {
+            l("url not found in json: " + $json);
+        }
+    } else {
+        l(__FUNCTION__." status :".$status);
+    }
+    return null;
+}
+
+function download_chunklist() {
+    global $chunklist_list, $effective_url, $chunklist_url;
+
+    // try download the chunk list
+    l("getting chunk list");
+    if ($chunklist_url) {
+        $c = new_curl($chunklist_url);
+        curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+        $chunklist = curl_exec($c);
+        $status = curl_getinfo($c, CURLINFO_HTTP_CODE);
+        if ($status == 200) {
+            return $chunklist;
+        }
+    }
+
+    // if we can't download the chunk list, start over
+    l("getting cdn url");
+    $cdn_url = get_cdn_url();
+    l("cdn url: ".$cdn_url);
+
+    if (!$cdn_url) {
+        // if we can't even get this, there's nothing more we can do
+        exit(1);
+    }
+
+    l("getting list of chunk list urls");
+    $chunklist_list_and_effective_url = get_chunklist_list_and_effective_url($cdn_url);
+    if ($chunklist_list_and_effective_url) {
+        $chunklist_list = $chunklist_list_and_effective_url[0];
+        $effective_url = $chunklist_list_and_effective_url[1];
+        $parsed_chunklist_list = parse_chunklist_list($chunklist_list);
+        $chunklist_filename = $parsed_chunklist_list[count($parsed_chunklist_list)-1]["filename"];
+        $chunklist_url = str_replace("playlist.m3u8", $chunklist_filename, $effective_url);
+        l("chunklist url: ".$chunklist_url);
+    } else {
+        // Can't get chunk list URLs, should not happen
+        exit(1);
+    }
+
+    l("getting chunk list again");
     $c = new_curl($chunklist_url);
     curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
     $chunklist = curl_exec($c);
@@ -49,22 +117,32 @@ function download_chunklist($chunklist_url) {
     if ($status == 200) {
         return $chunklist;
     } else {
-        l(__FUNCTION__." status :".$status);
-        return null;
+        l("status: ".$status);
+        exit(1);
     }
 }
 
 function download_chunks($chunklist, $effective_url) {
-    global $prefix, $filetime;
+    global $db, $prefix, $filetime;
     $mh = curl_multi_init();
     $connections = array();
     $files = array();
     curl_multi_setopt($mh, CURLMOPT_PIPELINING, 1);
     curl_multi_setopt($mh, CURLMOPT_MAXCONNECTS, 1);
     foreach($chunklist as $chunk) {
+        if (db_is_file_downloaded($db, $chunk["filename"])) {
+            // this file is already downloaded, skip downloading
+            l("skip downloading: : ".$chunk["filename"]);
+
+            // fill in the corresponding array elements to make these arrays synchronized with $chunklist
+            $connections[] = null;
+            $files[] = null;
+            continue;
+        }
+        l("going to download: ".$chunk["filename"]);
         $url = str_replace("playlist.m3u8", $chunk["filename"], $effective_url);
         $c = new_curl($url);
-        $f = fopen(DATA_DIR.$prefix."-".intval($filetime).".ts", "w");
+        $f = fopen(DATA_DIR.$chunk["filename"], "w");
 
         curl_setopt($c, CURLOPT_FILE, $f);
         curl_multi_add_handle($mh, $c);
@@ -72,7 +150,6 @@ function download_chunks($chunklist, $effective_url) {
         $connections[] = $c;
         $files[] = $f;
 
-        $filetime += $chunk["duration"];
     }
 
     //execute the handles
@@ -81,14 +158,48 @@ function download_chunks($chunklist, $effective_url) {
         $mrc = curl_multi_exec($mh, $still_running);
         curl_multi_select($mh, 5 /* wait x second */);
     } while ($still_running > 0);
+    l("all downloads finished");
 
-    foreach($connections as $c) {
-        curl_multi_remove_handle($mh, $c);
+    $t = $filetime;
+    for($i = 0; $i < count($connections); $i++) {
+        $c = $connections[$i];
+        $chunk = $chunklist[$i];
+        if ($c) {
+            curl_multi_remove_handle($mh, $c);
+            $status = curl_getinfo($c, CURLINFO_HTTP_CODE);
+            if ($status == 200) {
+                db_add_file($db, $t, $chunk["filename"], $chunk["duration"]);   
+            } else {
+                l("chunk download failed, status: ".$status.", filename: ".$chunk["filename"]);
+            }
+            $t += $chunk["duration"];
+        }
     }
+    $filetime = $t;
     foreach($files as $f) {
-        fclose($f);
+        if ($f) {
+            fclose($f);
+        }
     }
     curl_multi_close($mh);
+}
+
+function db_is_file_downloaded(&$db, $filename) {
+    foreach ($db as $row) {
+        if ($row["filename"] == $filename) {
+            return true;
+        }
+    }
+    return false;
+}
+function db_add_file(&$db, $t, $filename, $duration) {
+    $db[] = array(
+        "time" => $t,
+        "filename" => $filename,
+        "duration" => $duration
+    );
+}
+function db_save($db) {
 }
 
 function parse_chunklist($chunklist) {
@@ -165,30 +276,6 @@ function insert_sorted($array, $element, $sort_key) {
     return array_merge($array, array($element));
 }
 
-function get_cdn_url() {
-    global $url;
-    l(__FUNCTION__."()");
-
-    $c = new_curl($url."?feed");
-    curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($c, CURLOPT_POST, true);
-    curl_setopt($c, CURLOPT_POSTFIELDS, "feed");
-    $json = curl_exec($c);
-    $status = curl_getinfo($c, CURLINFO_HTTP_CODE);
-    curl_close($c);
-
-    if ($status == 200) {
-        $feedinfo = json_decode($json, true);
-        if (isset($feedinfo["url"])) {
-            return $feedinfo["url"];
-        } else {
-            l("url not found in json: " + $json);
-        }
-    } else {
-        l(__FUNCTION__." status :".$status);
-    }
-    return null;
-}
 function new_curl($url) {
     global $cookie_jar_path;
     $c = curl_init();
@@ -206,4 +293,5 @@ function l($log){
     print $log."\n";
 }
 
+init();
 fetch_loop();
